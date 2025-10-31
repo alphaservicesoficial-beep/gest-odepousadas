234ce11d0e408113373f61760b3faebe37fcaae3
Gabriel090207 <gabrielborela07@gmail.com>
2025-10-31 12:47:37 -0300

    Adapta Firebase para usar variável de ambiente

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -1,14 +1,17 @@
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
+import json
 import os
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
-# Caminho do arquivo JSON
-cred_path = os.path.join(os.path.dirname(__file__), "firebase-key.json")
+firebase_key_str = os.getenv("FIREBASE_KEY")
 
-# Inicializa o Firebase Admin
-if not firebase_admin._apps:
+if firebase_key_str:
+    firebase_key = json.loads(firebase_key_str)
+    cred = credentials.Certificate(firebase_key)
+else:
+    # Fallback local
+    cred_path = os.path.join(os.path.dirname(__file__), "firebase-key.json")
     cred = credentials.Certificate(cred_path)
-    firebase_admin.initialize_app(cred)
 
-# Instância global do Firestore
+firebase_admin.initialize_app(cred)
 db = firestore.client()
